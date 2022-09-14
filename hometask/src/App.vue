90926dad7290bd4e3fef8270937ac2acd6f5d216
lesha13 <85362539+lesha13@users.noreply.github.com>
2022-09-14 10:54:30 +0300

    added

---
+++ hometask/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <product-element
+      v-for="item in ProductData.data"
+      :key="item.id"
+      :imgSrc="item.imgSrc"
+      :header="item.header"
+      :extraInfo="item.extraInfo"
+      :price="item.price"
+    />
+  </div>
+</template>
+
+<script>
+import ProductElement from "./components/ProductElement/index.vue";
+import ProductData from "./assets/data.json";
+
+export default {
+  name: "App",
+
+  components: {
+    ProductElement,
+  },
+
+  data() {
+    return {
+      ProductData,
+    };
+  },
+};
+</script>
+
+<style>
+.container {
+  width: 1000px;
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ hometask/src/components/ProductElement/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="elem">
+    <img :src="imgSrc" />
+    <p>{{ header }}</p>
+    <p>{{ extraInfo }}</p>
+    <p>{{ price }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductElement",
+
+  props: {
+    imgSrc: {
+      type: String,
+      default:
+        "https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png",
+    },
+    header: {
+      type: String,
+      default: "None",
+    },
+    extraInfo: {
+      type: String,
+      default: "None",
+    },
+    price: {
+      type: String,
+      default: "None",
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.elem {
+  display: flex;
+  flex-direction: column;
+}
+</style>
