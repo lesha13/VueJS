3fa5ba4995e89020a6139b3f6e45222b9a7d6c8a
lesha13 <leshamashuna@gmail.com>
2022-09-22 09:13:04 +0300

    aaa

---
+++ stocks/src/pages/MyAccountPage/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>My account</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MyAccountPage"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ stocks/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+import HomePage from "../pages/HomePage"
+import MyAccountPage from "../pages/MyAccountPage"
+import AboutPage from "../pages/AboutPage"
+
+const routes = [
+    {
+        path: "/home",
+        component: HomePage,
+        name: "HomePage"
+    },
+    {
+        path: "/account",
+        component: MyAccountPage,
+        name: "MyAccountPage"
+    },
+    {
+        path: "/about",
+        component: AboutPage,
+        name: "AboutPage"
+    },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+export default router;
